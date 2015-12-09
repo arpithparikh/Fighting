31bce7a48af8318d62bf62f33ecf4996dbeb85af
JamesJi9277 <jiqi@gwu.edu>
2015-12-09 16:43:51 -0500

    12/09/2015

--- bb-minStack.java
+++ bb-minStack.java
@@ -32,3 +32,29 @@ class MinStack {
         return minStack.peek();
     }
 }
+
+简化后的代码
+class MinStack {
+    Stack<Integer> stack = new Stack<Integer>();
+    Stack<Integer> minStack = new Stack<Integer>();
+    public void push(int x) {
+        stack.push(x);
+        if(minStack.isEmpty() || minStack.peek() >= x) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if(stack.pop().equals(minStack.peek())) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
