50185fa6e2c4ac0222f6041871e45051ae3ebcf7
JamesJi9277 <jiqi@gwu.edu>
2015-12-12 20:41:58 -0500

    12/12/2015

--- bb-printIntegerWithComma.java
+++ bb-printIntegerWithComma.java
@@ -1,13 +1,18 @@
-public class Solution {
-	public void printWithComma(int nums) {
+
+public class print {
+	public void printWithComma(int num) {
 		StringBuffer sb = new StringBuffer();
+		int count = 0;
 		while(num > 0) {
 			int digit = num % 10;
-			if(sb.length() % 3 != 0) {
+			if((sb.length() - count) % 3 != 0) {
 				sb.append(digit + "");
 			}
-			else {
-				sb.append(",");
+			else if((sb.length() - count) % 3 == 0){
+				if(sb.length() != 0) {
+					sb.append(",");
+					count++;
+				}
 				sb.append(digit + "");
 			}
 			num /= 10;
@@ -17,8 +22,12 @@ public class Solution {
 		for(int i = 0; start + i < end - i; i++) {
 			char temp = sb.charAt(start + i);
 			sb.setCharAt(start + i, sb.charAt(end - i));
-			sb.setCharAt(end - i, sb.charAt(start + i));
+			sb.setCharAt(end - i, temp);
 		}
-		return sb.toString();
+		System.out.println(sb.toString());
+	}
+	public static void main(String[] args) {
+		print p = new print();
+		p.printWithComma(10009999);
 	}
 }
