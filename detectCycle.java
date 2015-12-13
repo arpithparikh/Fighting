04a80477e1d5539bec6cc00bab2a8cd2dc3f7ce5
JamesJi9277 <jiqi@gwu.edu>
2015-12-13 17:05:27 -0500

    12/13/2015

--- detectCycle.java
+++ detectCycle.java
@@ -131,6 +131,51 @@ public class TestCycle {
     }
 }
 
+时间复杂度O(v + e)
+public class Solution {
+    public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        ArrayList<DirectedGraphNode> res = new ArrayList<DirectedGraphNode>();
+        if(graph == null || graph.size() == 0) {
+            return res;
+        }
+        if(graph.size() == 1) {
+            return graph;
+        }
+        HashMap<DirectedGraphNode, Integer> map = new HashMap<DirectedGraphNode, Integer>();
+        Queue<DirectedGraphNode> queue = new LinkedList<DirectedGraphNode>();
+        for(DirectedGraphNode node : graph) {
+            for(DirectedGraphNode neighbor : node.neighbors) {
+                if(map.containsKey(neighbor)) {
+                    map.put(neighbor, map.get(neighbor) + 1);
+                }
+                else {
+                    map.put(neighbor, 1);
+                }
+            }
+        }
+        for(DirectedGraphNode node : graph) {
+            if(!map.containsKey(node)) {
+                queue.offer(node);
+                res.add(node);
+            }
+        }
+        while(!queue.isEmpty()) {
+            DirectedGraphNode node = queue.poll();
+            for(DirectedGraphNode neighbor : node.neighbors) {
+                map.put(neighbor, map.get(neighbor) - 1);
+                if(map.get(neighbor) == 0) {
+                    queue.offer(neighbor);
+                    res.add(neighbor);
+                }
+            }
+        }
+        return res;
+        //这里还可以说 
+        if(res.size() < graph.size()) {
+            return false; => exist Cycle
+        }
+    }
+}
 
 
 
