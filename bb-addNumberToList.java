bfe10441eb1f4143a22e64d6e1a4f4530518cad4
JamesJi9277 <jiqi@gwu.edu>
2015-12-13 10:43:24 -0500

    12/13/2015

--- bb-addNumberToList.java
+++ bb-addNumberToList.java
@@ -9,35 +9,61 @@
 
 ex. [30, 5], 则最新加入的数只能为25, list变为[30, 5, 25] 继续, 
 只能再加入20, list成为[30, 5, 25, 20], 接着就有两种选择,
- 可以加10(30-20) 或是15(20-5). 于是会分出两个branch [30, 5, 25, 20, 10] 跟[30, 5, 25, 20, 15], 
- 然后再把最后一个可能补上之后变成 [30, 5, 25, 20, 10, 15]跟[30, 5, 25, 20, 15, 10]
+可以加10(30-20) 或是15(20-5). 于是会分出两个branch [30, 5, 25, 20, 10] 跟[30, 5, 25, 20, 15], 
+然后再把最后一个可能补上之后变成 [30, 5, 25, 20, 10, 15]跟[30, 5, 25, 20, 15, 10]
 
- public class Solution {
- 	public ArrayList<ArrayList<Integer>> find(int[] nums) {
- 		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
- 		if(nums == null || nums.length < 2) {
- 			return res;
- 		}
- 		ArrayList<Integer> temp = new ArrayList<Integer>();
- 		temp.add(nums[0]);
- 		temp.add(nums[1]);
- 		helper(res, temp);
- 		return res;
- 	}
- 	private void helper(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> temp) {
- 		HashSet<Integer> set = new HashSet<Integer>();
- 		for(int i = 0; i < temp.size(); i++) {
- 			int candidate = Math.abs(temp.get(i) - temp.get(temp.size() - 1));
- 			if(!temp.contains(candidate) && !set.contains(candidate)) {
- 				temp.add(candidate);
- 				set.add(candidate);
- 				helper(res, temp);
- 				set.remove(candidate);
- 				temp.remove(temp.size() - 1);
- 			}
- 		}
- 		if(set.size() == 0) {
- 			res.add(new ArrayList<Integer>(temp));
- 		}
- 	}
+import java.util.*;
+public class add {
+
+public static ArrayList<ArrayList<Integer>> genNums(int num1, int num2) {
+        ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
+
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        result.add(num1);
+        result.add(num2);
+        Queue<ArrayList<Integer>> queue = new LinkedList<>();
+        queue.offer(result);
+        genNumsHelper(results, queue);
+        return results;
+    }
+
+    public static void genNumsHelper(ArrayList<ArrayList<Integer>> results, Queue<ArrayList<Integer>> queue) {
+        while(queue.size() > 0) {
+            boolean find = false;
+            ArrayList<Integer> result = queue.poll();
+            int size = result.size();
+            Set<Integer> set = new HashSet<>(result);
+            for (int i = 0; i < size - 1; i++) {
+                int diff = Math.abs(result.get(i) - result.get(size - 1));
+                if (set.contains(diff)) {
+                    continue;
+                } else {
+                    find = true;
+                }
+                set.add(diff);
+                result.add(diff);
+                queue.offer(new ArrayList<Integer>(result));
+                for(Integer item : result) {
+                	System.out.print(item + " ");
+                }
+                System.out.println(" ");
+                result.remove(result.size() - 1);
+            }
+            if (!find) results.add(new ArrayList<Integer>(result));
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> results = genNums(30, 5);
+    }
  }
+
+30 5 25  
+30 5 25 20  
+30 5 25 20 10  
+30 5 25 20 15  
+30 5 25 20 10 15  
+30 5 25 20 15 10  
+
+
