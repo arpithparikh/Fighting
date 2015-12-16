6d37f38b2c68bf44c9359c9c3c01288a54499b13
JamesJi9277 <jiqi@gwu.edu>
2015-12-16 17:59:03 -0500

    12/16/2015

--- bb-stringChange.java
+++ bb-stringChange.java
@@ -1,42 +1,58 @@
-//a#3bd#5 -》 aaabddddd
-public class change1 {
-	public String change(String s){
+a#3bd#5 -》 aaabddddd
+public class change {
+	public String change(String s) {
 		if(s == null || s.length() == 0) {
-			return new String();
+			return s;
 		}
-		int left = 0;
-		int right = 0;
 		StringBuffer sb = new StringBuffer();
-		for(right = 0; right < s.length(); ) {
-			if(s.charAt(right) >= 'a' && s.charAt(right) <= 'z') {
-				right++;
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == '#') {
+				continue;
 			}
-			else if(s.charAt(right) == '#') {
-				sb.append(s.substring(left, right));
-				char c = s.charAt(right - 1);
-				right++;
-				int numStart = right;
-				while(right < s.length() && s.charAt(right) >= '0' && s.charAt(right) <= '9') {
-					right++;
+			else {
+				sb.apppend(s.charAt(i));
+			}
+		}
+		if(sb.length() == 1) {
+			return sb.toString();
+		}
+		StringBuffer res = new StringBuffer();
+		int left = 0;
+		int right = 1;
+		while(right < sb.length()) {
+			if(isChar(s.charAt(left)) && isNumber(s.charAt(right))) {
+				int count = s.charAt(right) - '0';
+				while(count-- > 0) {
+					res.append(sb.charAt(left));
 				}
-				int count = Integer.parseInt(s.substring(numStart, right));
-				helper(sb, count, c);
+				left += 2;
+				right+= 2;
+			}
+			if(isChar(s.charAt(left)) && isChar(s.charAt(right))) {
+				res.append(sb.charAt(left));
 				left = right;
+				right++;
 			}
 		}
-		if(left < s.length()) {
-			sb.append(s.substring(left, right));
-		}
-		return sb.toString();
+		return res.toString();
 	}
-	private void helper(StringBuffer sb, int count, char c) {
-		while(--count > 0) {
-			sb.append(c);
+	private boolean isChar(char c) {
+		if((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A')) {
+			return true;
 		}
+		else {
+			return false;
+		}
+	}
+	private boolean isNumber(char c) {
+		if(c <= '9' && c >= '0') {
+			return true;
+		}
+		return false;
 	}
 	public static void main(String[] args) {
-		change1 c = new change1();
-		String s = "a#3bd#10";
+		change c = new change();
+		String s = "a#3bd#5";
 		System.out.print(c.change(s));
 	}
 }
