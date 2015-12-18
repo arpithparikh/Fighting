25cbad97c38735baa5fa2a875fc639446aab9557
JamesJi9277 <jiqi@gwu.edu>
2015-12-18 14:15:53 -0500

    12/18/2015

--- bb-decodingString.java
+++ bb-decodingString.java
@@ -1,27 +1,43 @@
 a#3bd#5 -》 aaabddddd
 
-
-public class decoding {
-	public String decoding(String s) {
-		if(s == null || s.length() < 3) {
+public class change1 {
+	public String change(String s){
+		if(s == null || s.length() == 0) {
 			return new String();
 		}
+		int left = 0;
+		int right = 0;
 		StringBuffer sb = new StringBuffer();
-		for(int i = 0; i < s.length(); i++) {
-			if(s.charAt(i) == '#') {
-				char temp = s.charAt(i - 1);
-				int count = s.charAt(i + 1) - '0';
-				while(count-- > 0) {
-					sb.append(temp);
+		for(right = 0; right < s.length(); ) {
+			if(s.charAt(right) >= 'a' && s.charAt(right) <= 'z') {
+				right++;
+			}
+			else if(s.charAt(right) == '#') {
+				sb.append(s.substring(left, right));
+				char c = s.charAt(right - 1);
+				right++;
+				int numStart = right;
+				while(right < s.length() && s.charAt(right) >= '0' && s.charAt(right) <= '9') {
+					right++;
 				}
-				i += 2;
+				int count = Integer.parseInt(s.substring(numStart, right));
+				helper(sb, count, c);
+				left = right;
 			}
 		}
+		if(left < s.length()) {
+			sb.append(s.substring(left, right));
+		}
 		return sb.toString();
- 	}
+	}
+	private void helper(StringBuffer sb, int count, char c) {
+		while(--count > 0) {
+			sb.append(c);
+		}
+	}
 	public static void main(String[] args) {
-		decoding d = new decoding();
-		String s = "a#3b#1d#5";
-		System.out.println(d.decoding(s));
+		change1 c = new change1();
+		String s = "a#3bd#10";
+		System.out.print(c.change(s));
 	}
 }
