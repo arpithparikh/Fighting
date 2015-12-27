6d0f537eb011e92ff96ba7c3d2aba8f95c367766
JamesJi9277 <jiqi@gwu.edu>
2015-12-27 11:51:44 -0500

    12/27/2015

--- bb-wildcardMatching.java
+++ bb-wildcardMatching.java
@@ -55,3 +55,39 @@ public class Solution {
 		return true;
 	}
 }
+
+public class Solution {
+    /**
+     * @param s: A string 
+     * @param p: A string includes "?" and "*"
+     * @return: A boolean
+     */
+    public boolean isMatch(String s, String p) {
+        // write your code here
+        int i = 0;
+        int j = 0;
+        int star = -1;
+        int mark = -1;
+        while(i < s.length()) {
+        	if(j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+        		i++;
+        		j++;
+        	}
+        	else if(j < p.length() && p.charAt(j) == '*') {
+        		start = j++;
+        		mark = i;
+        	}
+        	else if(star != -1) {
+        		j = star + 1;
+        		i = ++mark;
+        	}
+        	else {
+        		return false;
+        	}
+        }
+        while(j < p.length() && p.charAt(j) == '*') {
+        	j++;
+        }
+        return j == p.length();
+     }
+}
