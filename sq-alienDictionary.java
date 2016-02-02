d14457f3d5e824841d20140b8dc0be53242f9b4b
JamesJi9277 <jiqi@gwu.edu>
2016-02-02 18:15:42 -0500

    02/02/2016

--- sq-alienDictionary.java
+++ sq-alienDictionary.java
@@ -1,66 +1,68 @@
 public class Solution {
-	public String alienOrder(String[] words) {
-		//step1 construct graph
-		//step2 construct order
-		//step3 traverse graph
-
-		//initail graph
-		HashMap<Character, HashSet<Character>> map = new HashMap<Character, HashSet<Character>>();
-		for(int i = 0; i < words.length; i++) {
-			for(int j = 0; j < words[i].length(); j++) {
-				if(!map.containsKey(words[i].charAt(j))) {
-					map.put(words[i].charAt(j), new HashSet<Character>());
-				}
-			}
-			if(i > 0) {
-				order(map, words[i - 1], words[i]);
-			}
-		}
-		return topoSort(map);
-	}
-	public String topoSort(HashMap<Character, HashSet<Character>> graph) {
-		Queue<Character> queue = new LinkedList<Character>();
-		HashMap<Character, Integer> indegree = new HashMap<Character, Integer>();
-		StringBuffer sb = new StringBuffer();
-		int numNode = 0;
-		for(char c : graph.keySet()) {
-			for(char a : graph.get(c)) {
-				int count = indegree.containsKey(a) ? indegree.get(a) + 1 : 1;
-				indegree.put(a, count);
-			}
-		}
-		for(char c : graph.keySet()) {
-			if(!indegree.containsKey(c)) {
-				queue.offer(c);
-			}
-		}
-		while(!queue.isEmpty()) {
-			char c = queue.poll();
-			sb.append(c);
-			numNode++;
-			for(char a : graph.get(c)) {
-				indegree.put(a, indegree.get(a) - 1);
-				if(indegree.get(a) == 0) {
-					queue.offer(a);
-				}
-			}
-		}
-		if(numNode != graph.size()) {
-			return "";
-		}
-		return sb.toString();
-	}
-	private void order(HashMap<Character, HashSet<Character>> map, String prev, String cur) {
-		int minLength = Math.min(prev.length(), cur.length());
-		for(int i = 0; i < minLength; i++) {
-			char p = prev.charAt(i);
-			char c = cur.charAt(i);
-			if(p != c) {
-				if(!map.get(p).contains(c)) {
-					map.get(p).add(c);
-				}
-				break;
-			}
-		}
-	}
+    public String alienOrder(String[] words) {
+        if(words == null || words.length == 0) {
+        	return "";
+        }
+        HashMap<Character, HashSet<Character>> graph = new HashMap<Character, HashSet<Character>>();
+        for(int i = 0; i < words.length; i++) {
+        	for(int j = 0; j < words[i].length(); j++) {
+        		if(!graph.containsKey(words[i].charAt(j))) {
+        			graph.put(words[i].charAt(j), new HashSet<Character>());
+        		}
+        	}
+        }
+        for(int i = 0; i < words.length - 1; i++) {
+            getOrder(graph, words[i], words[i + 1]);
+        }
+        return topoSort(graph);
+    }
+    private void getOrder(HashMap<Character, HashSet<Character>> graph, String prev, String cur) {
+    	int minLength = Math.min(prev.length(), cur.length());
+    	for(int i = 0; i < minLength; i++) {
+    		char p = prev.charAt(i);
+    		char c = cur.charAt(i);
+    		if(p != c) {
+    			if(!graph.get(p).contains(c)) {
+    				graph.get(p).add(c);
+    			}
+    			break;
+    		}
+    	}
+    }
+    private String topoSort(HashMap<Character, HashSet<Character>> graph) {
+    	Queue<Character> queue = new LinkedList<Character>();
+    	HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    	int numNode = 0;
+    	StringBuffer sb = new StringBuffer();
+    	for(Character c : graph.keySet()) {
+    		for(char a : graph.get(c)) {
+    			if(!map.containsKey(a)) {
+    				map.put(a, 1);
+    			}
+    			else {
+    				map.put(a, map.get(a) + 1);
+    			}
+    		}
+    	}
+    	for(Character c : graph.keySet()) {
+    		if(!map.containsKey(c)) {
+    			queue.offer(c);
+    		}
+    	}
+    	while(!queue.isEmpty()) {
+    		Character c = queue.poll();
+    		numNode++;
+    		sb.append(c);
+    		for(Character a : graph.get(c)) {
+    			map.put(a, map.get(a) - 1);
+    			if(map.get(a) == 0) {
+    				queue.offer(a);
+    			}
+    		}
+    	}
+    	if(numNode != graph.size()) {
+    		return "";
+    	}
+    	return sb.toString();
+    }
 }
